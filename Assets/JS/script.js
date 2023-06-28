c40043156dd6b6b4dac92e936f23c8b8ad1c16c4
vikhyat gupta <108997136+Vikhyatz@users.noreply.github.com>
2023-06-28 22:32:07 +0530

    updated

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,17 +1,20 @@
 // intializing home and playlist links
 let home = document.getElementById('home')
 let playlist = document.getElementById('playlist')
+let heroHead = document.getElementById('heroHead')
 
-// initializing the containers rispective to the above links
-let homeContainer = document.getElementById('heroGrid')
+// initializing the containers respective to the above links
+let homeContainer = document.getElementById('gridContainer')
 let playlistContainer = document.getElementById('tableContainer')
 
-home.addEventListener('click',()=>{
-    homeContainer.style.display = 'block';
+home.addEventListener('click', () => {
+    homeContainer.style.display = 'grid';
     playlistContainer.style.display = 'none';
+    heroHead.innerText = 'Home'
 })
 
-playlist.addEventListener('click',()=>{
+playlist.addEventListener('click', () => {
     playlistContainer.style.display = 'block';
     homeContainer.style.display = 'none';
+    heroHead.innerText = 'Your Playlist'
 })

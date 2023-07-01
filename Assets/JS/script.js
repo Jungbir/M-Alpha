c431df025deab061ae6b0d277eaef0bffdf218df
vikhyat gupta <108997136+Vikhyatz@users.noreply.github.com>
2023-07-01 17:37:44 +0530

    updated

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,6 +3,7 @@ let home = document.getElementById('home')
 let searchForm = document.getElementById('form') //changed the id to the search bar id
 let heroHead = document.getElementById('heroHead')
 let searchInput = document.getElementById('navSearch')
+let musicPlayer = document.getElementById('musicPlayer')
 
 let searchIcon = document.getElementById('searchIcon')
 
@@ -44,9 +45,25 @@ function randomSongsGen() {
     // geting random songs and displying on load in the home page
     async function _random(token) {
 
-        let page = Math.floor(Math.random() * 90);
+        // random alphabet will be chosen from this variable
+        const characters = 'abcdefghijklmnopqrstuvwxyz';
 
-        const result = await fetch(`https://api.spotify.com/v1/search?q=getRandomSearch%28%29&type=track&offset=${page}`, {
+        // random alphabet chooser function
+        function generateString(length) {
+            let result = ' ';
+            const charactersLength = characters.length;
+            for (let i = 0; i < length; i++) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+
+            return result;
+        }
+
+        // random offset
+        let offset = Math.floor(Math.random() * 90);
+
+        // endpoint for random song generator
+        const result = await fetch(`https://api.spotify.com/v1/search?q=${generateString(1)}%28%29&type=track&offset=${offset}`, {
             method: 'GET',
             headers: { 'Authorization': 'Bearer ' + token }
         });
@@ -55,19 +72,34 @@ function randomSongsGen() {
         console.log('random songs:', data.tracks.items)
 
         data.tracks.items.forEach((songs) => {
+            // creating the element in which the images, title,etc will be displayed
             const div = document.createElement("div")
             div.classList.add('gridItem')
             homeContainer.appendChild(div)
 
+            // DOM manipulating the random songs tracks fetched by the endpoint
             div.innerHTML = `
             <div class="gridItem">
-            <div class="gridImg"><img src="${songs.album.images[1].url}" alt=""></div>
-            <div class="gridInfo">
-            <div id="gridName">${songs.name}</div>
-            <div id="gridArtist">${songs.artists[0].name}</div>
-            </div>
+                <div class="gridImg"><img src="${songs.album.images[1].url}" alt=""></div>
+                <div class="gridInfo">
+                    <div id="gridName">${songs.name}</div>
+                    <div id="gridArtist">${songs.artists[0].name}</div>
+                </div>
             </div>
             `
+
+            div.addEventListener('click', () => {
+
+                musicPlayer.innerHTML = `
+                <iframe style="border-radius:12px"
+                    src="https://open.spotify.com/embed/track/${songs.id}?utm_source=generator&theme=0"
+                    width="100%" height="100" frameBorder="0" allowfullscreen=""
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                    loading="lazy">
+                </iframe>
+                `
+            })
+
         })
     }
 
@@ -77,6 +109,7 @@ function randomSongsGen() {
 
         searchResult.innerText = 'loading....'
 
+        // endpoint for getting atmost 20 tracks on one search
         const result = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=track`, {
             method: 'GET',
             headers: { 'Authorization': 'Bearer ' + token }
@@ -88,6 +121,7 @@ function randomSongsGen() {
         searchResult.innerText = ''
 
         data.tracks.items.forEach((songs) => {
+            // creating the element in which the images, title,etc will be displayed
             const div = document.createElement("div")
             div.classList.add('srInfoContainer')
             searchResult.appendChild(div)
@@ -96,9 +130,10 @@ function randomSongsGen() {
             let durationMilli = songs.duration_ms
             let durationAllSeconds = durationMilli / 1000
             let durationMin = durationAllSeconds / 60
-            let minutes = Math.trunc(durationMin)
-            let seconds = Math.trunc((durationMin - minutes) * 60)
+            let minutes = Math.trunc(durationMin) // minutes variable for the song
+            let seconds = Math.trunc((durationMin - minutes) * 60) // seconds variable for the song
 
+            // DOM manipulating the search based songs tracks fetched by the endpoint
             div.innerHTML = `
                 <div class="sr1">
                     <img src="${songs.album.images[1].url}" alt="">
@@ -115,6 +150,19 @@ function randomSongsGen() {
                     <i class="fas fa-play"></i> Play
                 </button>
             `
+
+            div.addEventListener('click', () => {
+
+                musicPlayer.innerHTML = `
+                <iframe style="border-radius:12px"
+                    src="https://open.spotify.com/embed/track/${songs.id}?utm_source=generator&theme=0"
+                    width="100%" height="100" frameBorder="0" allowfullscreen=""
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                    loading="lazy">
+                </iframe>
+                `
+            })
+
         })
     }
 
@@ -122,8 +170,8 @@ function randomSongsGen() {
 
     // loading genres on load
     async function loadGeneres() {
-        const token = await _getToken();
-        const random = await _random(token);
+        const token = await _getToken(); // getting the token
+        const random = await _random(token); //getting random songs
     }
     loadGeneres()
 
@@ -143,8 +191,8 @@ function randomSongsGen() {
             heroHead.innerText = `search results for '${query}'`
 
             async function loadSearchResults() {
-                const token = await _getToken();
-                const search = await _search(token, query);
+                const token = await _getToken(); //getting the token
+                const search = await _search(token, query); // getting search results based on the query
             }
             loadSearchResults()
         }
@@ -163,8 +211,8 @@ function randomSongsGen() {
 
 
         async function loadSearchResults() {
-            const token = await _getToken();
-            const search = await _search(token, query);
+            const token = await _getToken(); //getting the token
+            const search = await _search(token, query); // getting search results based on the query
         }
         loadSearchResults()
 

122e2fa25eb1954bdc358f9fd249b3a5149590a7
vikhyat gupta <108997136+Vikhyatz@users.noreply.github.com>
2023-07-01 12:46:37 +0530

    updated

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -30,3 +30,72 @@ searchIcon.addEventListener('click', (e) => {
     searchResult.style.display = 'flex';
     heroHead.innerText = `search results for '${searchInput.value}'`
 })
+
+
+
+
+
+
+
+let gridContainer = document.getElementById('gridContainer')
+// let btn = document.getElementById('btn')
+
+// btn.addEventListener('click', () => {
+function randomSongsGen() {
+    const clientId = '380da68aac7a4f53b983f707f7158ddc';
+    const clientSecret = '2cfab4de8fd7469ea9e09cc051dc10f1';
+
+    async function _getToken() {
+        const result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+        });
+
+        const data = await result.json();
+        console.log(data.access_token)
+        let token = data.access_token
+        return token;
+    }
+
+
+    async function _random(token) {
+
+        let page = Math.floor(Math.random() * 90);
+
+        const result = await fetch(`https://api.spotify.com/v1/search?q=getRandomSearch%28%29&type=track&offset=${page}`, {
+            method: 'GET',
+            headers: { 'Authorization': 'Bearer ' + token }
+        });
+
+        const data = await result.json();
+        console.log('random songs:', data.tracks.items)
+
+        data.tracks.items.forEach((songs) => {
+            const div = document.createElement("div")
+            div.classList.add('gridItem')
+            gridContainer.appendChild(div)
+
+            div.innerHTML = `
+            <div class="gridItem">
+            <div class="gridImg"><img src="${songs.album.images[1].url}" alt=""></div>
+            <div class="gridInfo">
+            <div id="gridName">${songs.name}</div>
+            <div id="gridArtist">${songs.artists[0].name}</div>
+            </div>
+            </div>
+            `
+        }
+        )
+    }
+
+    async function loadGeneres() {
+        const token = await _getToken();
+        const random = await _random(token);
+    }
+    loadGeneres()
+
+}

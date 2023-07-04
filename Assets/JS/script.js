b62ebc7e5f0e142210ddf66a92d46976b4780cc8
vikhyat gupta <108997136+Vikhyatz@users.noreply.github.com>
2023-07-04 15:52:51 +0530

    update

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -150,19 +150,6 @@ function randomSongsGen() {
                     <i class="fas fa-play"></i> Play
                 </button>
             `
-
-            div.addEventListener('click', () => {
-
-                musicPlayer.innerHTML = `
-                <iframe style="border-radius:12px"
-                    src="https://open.spotify.com/embed/track/${songs.id}?utm_source=generator&theme=0"
-                    width="100%" height="100" frameBorder="0" allowfullscreen=""
-                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                    loading="lazy">
-                </iframe>
-                `
-            })
-
         })
     }
 
